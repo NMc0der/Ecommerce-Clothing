41ac87ddc44862d028414e95c7f70605bdeed3d0
Noah <noahglennmaldonado@gmail.com>
2024-03-29 06:54:15 -0400

    finished hero section and made it responsive

--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -1,17 +1,22 @@
+import { Link } from "react-router-dom";
 import fassionGirl from "../assets/PngItem_3977074.png";
 import Button from "./Button";
 
 const HeroHome = () => {
   return (
-    <section className="min-h-[650px] bg-red-300 flex items-center justify-center px-5 text-white">
-      <div className="self-end">
-        <img src={fassionGirl} alt="" />
+    <section className="min-h-[800px] bg-red-300 flex items-center justify-center px-5 text-white">
+      <div className="self-end ">
+        <img className="max-h-[700px]" src={fassionGirl} alt="" />
       </div>
-      <div className="hero-text">
-        <h2 className="font-Poppoins text-[2.5rem]">NEW ARRIVALS</h2>
+      <div className="hero-text absolute md:top-80 md:right-6  lg:static top-20 flex flex-col">
+        <h2 className="font-Poppoins text-[2.5rem] ">NEW ARRIVALS</h2>
         <h1 className="font-Courgette text-[3.5rem]">Summer Season!</h1>
-
-        <Button border={false} text={"SHOP NOW"} />
+        <Link
+          className="self-center sm:self-end md:self-center lg:self-start"
+          to={"/shop"}
+        >
+          <Button border={false} text={"SHOP NOW"} />
+        </Link>
       </div>
     </section>
   );
