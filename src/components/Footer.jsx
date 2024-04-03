d67dd89bc24473ca21996ad9dc9d7796701247d9
Noah <noahglennmaldonado@gmail.com>
2024-04-03 06:21:36 -0400

    added social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,37 @@
 import EmailSignUp from "./EmailSignUp";
+import { Link } from "react-router-dom";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaPinterest,
+  FaYoutube,
+} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <div>
       <EmailSignUp />
+      <div
+        className="h-[200px] flex items-center justify-center [&>a]:text-[40px] [&>a]:m-6 
+      [&>a]:cursor-pointer hover:[&>a]:text-[#FB9AD1]"
+      >
+        <Link to={"https://www.facebook.com/"}>
+          <FaFacebookF />
+        </Link>
+        <Link to={"https://www.instagram.com/"}>
+          <FaInstagram />
+        </Link>
+        <Link to={"https://twitter.com/?lang=en"}>
+          <FaXTwitter />
+        </Link>
+        <Link to={"https://www.pinterest.com/"}>
+          <FaPinterest />
+        </Link>
+        <Link to={"https://www.youtube.com/"}>
+          <FaYoutube />
+        </Link>
+      </div>
     </div>
   );
 };
