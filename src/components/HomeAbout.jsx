c39e2d59ce1cad93381d934102d388d5fb21503d
Noah <noahglennmaldonado@gmail.com>
2024-04-04 06:28:06 -0400

    added link to about page in homeAbout section

--- src/components/HomeAbout.jsx
+++ src/components/HomeAbout.jsx
@@ -1,10 +1,10 @@
 import Button from "./Button";
-
+import { Link } from "react-router-dom";
 const HomeAbout = () => {
   return (
     <div
       className="h-[550px] text-center  bg-[#EEEEEE] flex flex-col items-center
-    gap-1 justify-center tablet:gap-4 sm:gap-8 px-[10%] font-Garamond"
+    gap-1 justify-center tablet:gap-4 sm:gap-8 px-[10%] xl:px-[30%] font-Garamond"
     >
       <h2 className="tablet:text-2xl font-semibold">
         Can clothing change someones life?
@@ -22,9 +22,9 @@ const HomeAbout = () => {
         motivate us all to take on each new day feeling confident in what we
         wear and to change the lives of those around us through inspiration.
       </p>
-      <div className="text-white">
-        <Button text={"MORE ABOUT US"} />
-      </div>
+      <Link className="text-white border-[1px] border-black" to={"/about"}>
+        <Button border={false} text={"MORE ABOUT US"} />
+      </Link>
     </div>
   );
 };
