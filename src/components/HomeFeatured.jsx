2d690d1d28938b05156feed7299bc88089b1438e
Noah <noahglennmaldonado@gmail.com>
2024-04-09 12:17:25 -0400

    created responsive design for featured section

--- src/components/HomeFeatured.jsx
+++ src/components/HomeFeatured.jsx
@@ -6,11 +6,13 @@ import vouge from "../assets/Vogue-logo.png";
 import forbes from "../assets/Forbes-Logo-PNG-Pic.png";
 const HomeFeatured = () => {
   return (
-    <div className="min-h-[400px]">
-      <h2 className="text-center">AS SEEN IN</h2>
+    <div className="min-h-[400px] bg-[#EEEEEE] p-3 lg:p-12 2xl:p-8">
+      <h2 className="text-center text-xl sm:text-2xl font-semi-bold self-center ">
+        AS SEEN IN
+      </h2>
       <div
-        className="grid grid-cols-2 justify-center justify-items-center items-center
-      hover:[&>a]:scale-110 [&>a]:duration-100 lg:grid-cols-5"
+        className="grid grid-cols-2 justify-center justify-items-center items-center 
+      hover:[&>a]:scale-110 [&>a]:duration-100 xl:grid-cols-5 gap-2"
       >
         <Link>
           <img className=" w-[200px]" src={theTimes} alt="" />
@@ -24,7 +26,7 @@ const HomeFeatured = () => {
         <Link className="self-center">
           <img className=" w-[200px]" src={vouge} alt="" />
         </Link>
-        <Link className="col-span-2 lg:col-span-1">
+        <Link className="col-span-2 xl:col-span-1">
           <img className=" w-[200px]" src={forbes} alt="" />
         </Link>
       </div>
